62f986e309229fb40fc383b69bbb2497560ed202
DrOtter3000 <info@darknet-online.com>
2024-03-26 19:29:59 +0100

    add green door

---
+++ Levels/Environment/DoorGreen.gd
@@ -0,0 +1,19 @@
+extends Node3D
+
+
+@onready var sfxopen = $SFXopen
+
+var active: bool = true
+
+
+func _on_area_3d_body_entered(body):
+	if active:
+		if body.is_in_group("Player"):
+			if Gamestate.green_card == true:
+				active = false
+				sfxopen.play()
+				var tween : Tween
+				tween = create_tween()
+				tween.tween_property(self, "position:x", position.x +1.95, 3.2)
+	
+	
--- Scripts/gamestate.gd
+++ Scripts/gamestate.gd
@@ -4,3 +4,5 @@ var ammo := {
 	"pistol_rounds": 15,
 	"rifle_bullets": 10
 }
+
+var green_card: bool = true
